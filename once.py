34e65003b1a8bc51130d4bf00b665d6ee70a1589
yinguobing <yinguobing@gmail.com>
2024-10-31 12:15:26 +0800

    Show 3D bounding boxes in 3D view

--- once.py
+++ once.py
@@ -402,22 +402,23 @@ if __name__ == "__main__":
 
     # Create a blueprint
     blueprint = rrb.Blueprint(
-        rrb.Horizontal(
+        rrb.Vertical(
             contents=[
                 rrb.Spatial3DView(
-                    name=f"points",
-                    origin=f"/points",
+                    name=f"3D",
+                    origin=f"/",
                 ),
                 rrb.Grid(
+                    name="2D",
                     contents=[
                         rrb.Spatial2DView(
                             name=f"{cam_tag}",
                             origin=f"/images/{cam_tag}",
                         )
                         for cam_tag in dataset.camera_tags
-                    ]
+                    ],
                 ),
-            ]
+            ],
         )
     )
     rr.send_blueprint(blueprint)
@@ -425,6 +426,37 @@ if __name__ == "__main__":
     seq_id = list(dataset.train_split_list)[0]
     frame_ids = list(dataset.train_info[seq_id].keys())
     for frame_id in frame_ids:
+
+        # Labeled boxes
+        annos = dataset.get_frame_anno(seq_id, frame_id)
+        if annos is None:
+            continue
+
+        names, boxes_3d, _ = (
+            annos["names"],
+            np.array(annos["boxes_3d"]),
+            annos["boxes_2d"],
+        )
+        centers = boxes_3d[:, :3]
+        sizes = boxes_3d[:, 3:6]
+        rot_angles = [
+            rr.RotationAxisAngle([0, 0, 1], rr.Angle(r)) for r in boxes_3d[:, -1]
+        ]
+        colors = np.ones_like(centers, dtype=np.uint8)
+        colors[:, 1] *= 255
+        rr.set_time_sequence("main", int(frame_id))
+        rr.log(
+            "/boxes",
+            rr.Boxes3D(
+                sizes=sizes,
+                centers=centers,
+                colors=colors,
+                radii=0.01,
+                labels=names,
+                rotation_axis_angles=rot_angles,
+            ),
+        )
+
         # Point cloud
         points = dataset.load_point_cloud(seq_id, frame_id)
         rr.set_time_sequence("main", int(frame_id))
